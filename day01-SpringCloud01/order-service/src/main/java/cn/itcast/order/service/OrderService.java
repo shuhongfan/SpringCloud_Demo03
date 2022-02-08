ac2f1b9a1b2f6f2695034013c8f1cb6a05167b83
shuhongfan <shuhongfan@live.com>
2022-02-08 18:37:55 +0800

    day02-SpringCloud02 end

--- day01-SpringCloud01/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ day01-SpringCloud01/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,11 +1,11 @@
 package cn.itcast.order.service;
 
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -13,18 +13,26 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
 //        2.利用restTemplate发起http请求，查询用户
 //        2.1 url路径
-        String url = "http://user-service/user/"+order.getUserId();
+//        String url = "http://user-service/user/"+order.getUserId();
 //        2.2 发送http请求，实现远程调用
-        User user = restTemplate.getForObject(url, User.class);
+//        User user = restTemplate.getForObject(url, User.class);
 //        2.3分钟user到Order
+//        order.setUser(user);
+
+//        2.用Feign远程调用
+        User user = userClient.findById(order.getUserId());
+//        3.封装user到order
         order.setUser(user);
         // 4.返回
         return order;
--- day01-SpringCloud01/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ day01-SpringCloud01/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,11 +1,19 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//@RefreshScope
 @Slf4j
 @RestController
 @RequestMapping("/user")
@@ -14,6 +22,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
+    }
+
+    @GetMapping("prop")
+    public PatternProperties prop(){
+        return patternProperties;
+    }
+
     /**
      * 路径： /user/110
      *
@@ -21,7 +45,9 @@ public class UserController {
      * @return 用户
      */
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id) {
+    public User queryById(@PathVariable("id") Long id,
+                          @RequestHeader(value = "truth",required = false) String truth) {
+        System.out.println("truth:"+truth);
         return userService.queryById(id);
     }
 }
