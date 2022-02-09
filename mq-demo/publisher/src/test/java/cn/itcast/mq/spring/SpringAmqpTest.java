7a45d37f0b2925c2243c217717e33ec02e628a35
shuhongfan <shuhongfan@live.com>
2022-02-09 23:55:41 +0800

    day04-MQ end

---
+++ mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -0,0 +1,75 @@
+package cn.itcast.mq.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAmqpTest {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSendMessage2SimpleQueue(){
+        String queueName = "simple.queue";
+        String message ="hello, spring amqp!";
+        rabbitTemplate.convertAndSend(queueName,message);
+    }
+
+    @Test
+    public void testSendMessage2WorkQueue() throws InterruptedException {
+        String queueName = "simple.queue";
+        String message ="hello, spring amqp!";
+        for (int i = 0; i < 50; i++) {
+            rabbitTemplate.convertAndSend(queueName,message+i);
+            Thread.sleep(20);
+        }
+    }
+
+    @Test
+    public void testSendFanoutExchange(){
+//        交换机名称
+        String exchangeName = "shf.fanout";
+//        消息
+        String message = "hello,every one!";
+//        发送消息
+        rabbitTemplate.convertAndSend(exchangeName,"",message);
+    }
+
+    @Test
+    public void testSendDirectExchange(){
+//        交换机名称
+        String exchangeName = "shf.direct";
+//        消息
+        String message = "hello,blue!";
+//        发送消息
+        rabbitTemplate.convertAndSend(exchangeName,"blue",message);
+    }
+
+
+    @Test
+    public void testSendTopicExchange(){
+//        交换机名称
+        String exchangeName = "shf.topic";
+//        消息
+        String message = "hello,china.weather!";
+//        发送消息
+        rabbitTemplate.convertAndSend(exchangeName,"china.weather",message);
+    }
+
+    @Test
+    public void testSendObjectQueue(){
+        HashMap<String, Object> msg = new HashMap<>();
+        msg.put("name", "zhangsan");
+        msg.put("age", 18);
+        rabbitTemplate.convertAndSend("object.queue",msg);
+    }
+}
